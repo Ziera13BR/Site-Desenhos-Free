bf909e894095605f6a769d4570b93f6e94848f89
Ziera13BR <herouzi13ytbr@gmail.com>
2021-05-19 23:07:46 -0300

    time saver made and probabily working since i already made it but in another project.

---
+++ player/time-saver.js
@@ -0,0 +1,20 @@
+export function timeSaver(video, aberturafim, endingTime) {
+    var myPlayer = videojs(document.getElementById('video'))
+    myPlayer.on('playing', function () {
+        if (localStorage.getItem(video)) {
+            myPlayer.currentTime(Number(localStorage.getItem(video)))
+        } else {
+            var saving = setInterval(function () {
+                if (myPlayer.currentTime() >= endingTime) {
+                    clearInterval(saving)
+                    localStorage.removeItem(video)
+                } else {
+                    if (myPlayer.currentTime() > aberturafim) {
+                        localStorage.removeItem(video)
+                        localStorage.setItem(video, myPlayer.currentTime() - 2)
+                    }
+                }
+            }, 500)
+        }
+    })
+}
