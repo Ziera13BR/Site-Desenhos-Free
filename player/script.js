b01087857edbe245cfd402bc708a2a292b5edff4
Ziera13BR <herouzi13ytbr@gmail.com>
2021-05-19 21:56:01 -0300

    again: the return of those that didn't go

--- player/script.js
+++ player/script.js
@@ -1,13 +1,9 @@
 export default function runScript() {
-    import elementCreator from './element-creator.js'
-
     var url_string = window.location.href
     var url = new URL(url_string)
     var video = `videos/${url.searchParams.get("v")}.mp4`
     var aberturainicio = Number(url.searchParams.get("as"))
     var aberturafim = Number(url.searchParams.get("ae"))
-
-    elementCreator(video)
 
 
     var myPlayer = videojs(document.getElementById('video'))
